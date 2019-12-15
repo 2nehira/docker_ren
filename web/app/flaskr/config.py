29457c4daaf519dd61a004ff1fe0636614d70fbb
tunehira <kk.oak6@gmail.com>
2019-12-16 01:20:04 +0900

    all

---
+++ web/app/flaskr/config.py
@@ -0,0 +1,6 @@
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskr.db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tune:tune@localhost/flaskr'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tunehira@localhost/flaskr'
+
+SECRET_KEY = 'secret key'
---
+++ web/app/manage.py
@@ -0,0 +1,5 @@
+from flaskr import app
+# app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000, debug=True)
+
